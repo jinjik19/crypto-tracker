5d3365e8f073e730701ac31017ab1e73c088d1e3
Evgeniy Krasnikov <e.krasnikov@runity.ru>
2025-10-01 13:08:03 +0700

    add docker-compose

---
+++ initdb/create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS crypto_prices_daily (
+    fetch_date    DATE,
+    crypto_id     VARCHAR(50),
+    symbol        VARCHAR(50),
+    name          VARCHAR(100),
+    current_price NUMERIC(20, 10),
+    market_cap    BIGINT,
+    total_volume  BIGINT,
+    updated_at    TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY (fetch_date, crypto_id)
+);
