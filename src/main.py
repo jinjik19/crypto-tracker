cd32906289015c46dc0065ad81daf15d8fa72406
Evgeniy Krasnikov <e.krasnikov@runity.ru>
2025-10-01 12:48:55 +0700

    add logger

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from tenacity import RetryError
 
 from clients.coin_gecko_api import CoinGeckoApi, MarketParams, MarketResponse
 from config.settings import settings
+from utils.logger import logger
 
 
 def extract_data() -> list[MarketResponse]:
@@ -13,8 +14,10 @@ def extract_data() -> list[MarketResponse]:
     client = CoinGeckoApi(settings.api_key)
 
     try:
+        logger.debug("Try to fetch daily market data")
         return client.fetch_daily_market_data(params=MarketParams())
-    except RetryError:
+    except RetryError as e:
+        logger.warning(f"Cannot fetch data: {e}")
         return []
 
 
@@ -35,11 +38,15 @@ def save_data(cleaned_data: pd.DataFrame) -> None:
 
 def main() -> None:
     """Function is main entrypoint."""
+    logger.info("Start fetch data for top-10 cryptocurrency.")
     data = extract_data()
 
     if len(data) > 0:
+        logger.info("Transforming fetched data...")
         cleaned_data = transform_data(data)
+        logger.info("Saving cleaned data...")
         save_data(cleaned_data)
+        logger.info("Cryptocurrency data saved!")
 
 
 
