0fad55f6316e260a5a2c1d83d952e4a97ca48ba8
Evgeniy Krasnikov <e.krasnikov@runity.ru>
2025-10-01 12:01:15 +0700

    extract data

--- src/main.py
+++ src/main.py
@@ -1,6 +1,37 @@
 
-def main():
-    print("Hello, World!")
+import pandas as pd
+from tenacity import RetryError
+
+from clients.coin_gecko_api import CoinGeckoApi, MarketParams, MarketResponse
+from config.settings import settings
+
+
+def extract_data() -> list[MarketResponse]:
+    """Function extract data from coingecko."""
+    client = CoinGeckoApi(settings.api_key)
+
+    try:
+        return client.fetch_daily_market_data(params=MarketParams())
+    except RetryError:
+        return []
+
+
+def transform_data(data: list[MarketResponse]) -> pd.DataFrame:
+    """Function transform and clean data."""
+    df = pd.DataFrame([item.model_dump() for item in data])
+    # 1. format last_updated to timestamp
+    # 2. add new field fetch_date
+
+    return df
+
+
+def main() -> None:
+    """Function is main entrypoint."""
+    data = extract_data()
+
+    if len(data) > 0:
+        cleaned_data = transform_data(data)
+
 
 
 if __name__ == "__main__":
